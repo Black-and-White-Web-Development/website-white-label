ece7c9dc4783ffb85bf5caaf32a2b8eca0266a87
Chris <chris79gp@outlook.com>
2025-06-06 19:56:05 +0100

    Feat: add mobile navigaiton trigger using Radix `Dialog` component.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,17 @@
+import clsx from "clsx";
+
+import { useBreakpoint } from "@/hooks/useBreakpoint";
+
 import Navigation from "@/components/Navigation";
+import NavigationDialog from "@/components/NavigationDialog";
 import "./Header.scss";
 
 const Header = function () {
+	const isMobile = useBreakpoint();
+
 	return (
-		<header className="header fb-col-wrapper">
-			<Navigation />
+		<header className={clsx("header", { "fb-col-wrapper": !isMobile })}>
+			{isMobile ? <NavigationDialog /> : <Navigation />}
 		</header>
 	);
 };
